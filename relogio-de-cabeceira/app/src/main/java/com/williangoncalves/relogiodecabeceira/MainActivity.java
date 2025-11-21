06a13d0ebc54ba128e2730fd88042ec6491ddb6f
Willian Gonçalves <santoswil81@gmail.com>
2025-11-21 16:07:00 -0300

    animacao do menu de opcoes e preenchimento do horario

--- relogio-de-cabeceira/app/src/main/java/com/williangoncalves/relogiodecabeceira/MainActivity.java
+++ relogio-de-cabeceira/app/src/main/java/com/williangoncalves/relogiodecabeceira/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -17,9 +20,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.williangoncalves.relogiodecabeceira.databinding.ActivityMainBinding;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityMainBinding binding;
+    private final String TAG = "MainActivity";
+    private final Handler handler = new Handler();
+    private Runnable runnable;
     private final BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -43,13 +52,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
         registerReceiver(batteryReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        // Impede o bloqueio da tela automaticamente:
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        // App ocupa toda a extensão da tela:
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setFlags();
 
         setListeners();
         hideOptions();
+        startBedsideCLock();
     }
 
     @Override
@@ -63,15 +71,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (view.getId() == R.id.checkbox_battery_level) {
             toggleBatteryLevel();
         } else if (view.getId() == R.id.imageview_settings) {
-
+            showOptions();
         } else if (view.getId() == R.id.imageview_close) {
-
+            hideOptions();
         }
+    }
+
+    private void startBedsideCLock() {
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+                int second = calendar.get(Calendar.SECOND);
+
+                String hourMinuteFormat = String.format(Locale.getDefault(), "%02d:%02d", hour, minute);
+                String secondFormat = String.format(Locale.getDefault(), "%02d", second);
+
+                binding.textviewHourMinute.setText(hourMinuteFormat);
+                binding.textviewSeconds.setText(secondFormat);
+
+                long now = SystemClock.uptimeMillis();
+                Log.d(TAG, "Now: " + now);
+                handler.postAtTime(runnable, now + (1000 - (now % 1000)));
+                Log.d(TAG, "Now calculo: " + (now + (1000 - (now % 1000))));
+            }
+        };
+        runnable.run();
     }
 
     private void toggleBatteryLevel() {
         boolean isVisible = binding.textviewBatteryLevel.getVisibility() == View.VISIBLE;
         binding.textviewBatteryLevel.setVisibility(isVisible ? View.GONE : View.VISIBLE);
+    }
+
+    private void setFlags() {
+        // Impede o bloqueio da tela automaticamente:
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        // App ocupa toda a extensão da tela:
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     private void setListeners() {
@@ -81,6 +124,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void hideOptions() {
+        int duration = 400;
 
+        binding.checkboxBatteryLevel.post(new Runnable() {
+            @Override
+            public void run() {
+                int heightCheckbox = binding.checkboxBatteryLevel.getHeight();
+                binding.checkboxBatteryLevel.animate().translationY(heightCheckbox).setDuration(duration);
+            }
+        });
+
+        binding.imageviewClose.post(new Runnable() {
+            @Override
+            public void run() {
+                int heightImageClose = binding.imageviewClose.getHeight();
+                binding.imageviewClose.animate().translationY(heightImageClose).setDuration(duration);
+            }
+        });
+    }
+
+    private void showOptions() {
+        int duration = 400;
+        binding.checkboxBatteryLevel.animate().translationY(0).setDuration(duration);
+        binding.imageviewClose.animate().translationY(0).setDuration(duration);
     }
 }
